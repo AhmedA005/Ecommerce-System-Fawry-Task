05f5ee5416a93f2a06309b0800430d1e8387b407
ahmed <ahmed.ahmedmostafa.h@gmail.com>
2025-07-05 05:31:23 +0300

    Added digital product class for unshippable products.

---
+++ src/model/product/DigitalProduct.java
@@ -0,0 +1,8 @@
+package model.product;
+
+public class DigitalProduct extends BaseProduct{
+
+    public DigitalProduct(String name, int price, int quantity) {
+        super(name, price, quantity);
+    }
+}
